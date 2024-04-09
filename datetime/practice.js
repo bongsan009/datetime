6967b1c1ac036be6477f63bfe5e31c85897b51a9
bongsan009 <bbek2930@gmail.com>
2024-04-09 19:54:21 +0700

    add new feature

--- datetime/practice.js
+++ datetime/practice.js
@@ -4,15 +4,16 @@
 //Check if the input is a date object
 var isDate = function(input)
 {
-    // if(Object.prototype.toString.call(input) == "[object Date]")
-    // {
-    //     return true;
-    // }
+ 
 
-    if(typeof(input) == "date")
-    {
-        return "str"
-    }
+    if(Object.prototype.toString.call(input) == "[object Date]")
+        return true;
+
+    return false
+
 }
 
+//Test the function
 console.log(isDate(new Date()))
+console.log(isDate("text"));
+console.log(isDate(true));
